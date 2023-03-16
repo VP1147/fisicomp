d671ded335ecf23425e0d4f19fad579e41f8ad86
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-03-16 19:19:47 -0400

    Update raizes.py

--- FisiComp2/Aula1/raizes.py
+++ FisiComp2/Aula1/raizes.py
@@ -1,5 +1,7 @@
 # Encontrando Raizes: isolamento e refinamento
 
+import math as m
+
 # f(x) = x^2 = 4 = 0
 def f(x):
 	y = x**2 - 4
@@ -35,16 +37,54 @@ for x in range(-10, 10):
 		# print(x-1, g(x-1))
 		# print(x, g(x))
 		print("Raiz entre ",x-1," e",x)
-
 	x += 1
 
 ## Fazendo a derivada numerica nos intervalos encontrados
 dx = 0.2
-for x in range(-10, 10):
-	if g(x-1) * g(x) < 0:
-		xl = x - 1
-		while(xl < x):
-			print(xl, df(g, xl))
-			xl += dx
-	x += 1
+flag = 0
 
+def root(f): 
+	for x in range(-100, 100):
+		if g(x-1) * g(x) < 0:
+			xl = x - 1
+			print(">> ({:d},{:d})".format(x-1, x))
+			while(xl < x):
+				if df(g, xl) * df(g, xl + dx) < 0: flag += 1
+				if flag == 0: print("Apenas uma raiz em",xl,",", xl+dx)
+				xl += dx
+		x += 1
+
+def f(x):
+	return m.sqrt(x) - 5*m.e**(-x)
+
+def bissec(a, b, e, f): 					# Recebe um intervalo inicial [a,b],
+											# um épsilon 'e' e uma função f(x).
+
+	chi = (a+b)/2 							# chi: x aproximado, para cada iteração
+
+	i = 1 									# Contador de interações
+
+	while f(chi) > e or f(chi) < -e:		# Iterage enquanto chi não cumpre
+											# o requisito épsilon
+		if f(a)*f(chi) > 0: 
+			a = chi
+		elif f(a)*f(chi) < 0: 
+			b = chi
+		 
+		#print("Iteração:",i,">> [",a,",",b,"] 	| chi =", chi, "| f(chi) =",f(chi))
+		print("Iteraçao: {:d} >> [{:.6f},{:.6f}] | chi = {:.6f} | f(chi) = {:.6f}".format(i, a, b, chi, f(chi)))
+		chi = (a+b)/2
+		if i > 100: 
+			print(">> Limite de Iterações")
+			return a, b, chi, f(chi)
+		i+=1
+
+	print(">> Aproximação da raiz: x = {:.6f}".format(chi))
+	return a, b, chi, f(chi)  				# Retorna os valores de [a,b], chi e f(chi)
+
+a = 1.4
+b = 1.5
+bissec(a, b, 0.00001, f)
+
+def f(x):
+	return x**3 -9*x +3

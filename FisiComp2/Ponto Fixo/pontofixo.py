5acd9d03cd358fe075d0c8b448f27922409d16ea
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-04-07 15:10:28 -0400

    Update pontofixo.py

--- FisiComp2/Ponto Fixo/pontofixo.py
+++ FisiComp2/Ponto Fixo/pontofixo.py
@@ -5,9 +5,8 @@ def pfix(a, b, e, f, phi):					# Recebe um intervalo [a,b], uma
 	chi = (a+b) / 2  						# Aproximação inicial
 
 	i = 1   								# Contador de iterações
-	xn = chi
 	while abs(f(xn)) > e:
-		xn = phi(chi)
+		chi = phi(chi)
 		print("Iteraçao: {:d} >> [{:.6f},{:.6f}] | chi = {:.6f} | f(chi) = {:.6f}".format(i, a, b, chi, f(chi)))
 		i+=1
 	print("Iteraçao: {:d} >> [{:.6f},{:.6f}] | chi = {:.6f} | f(chi) = {:.6f}".format(i, a, b, chi, f(chi)))

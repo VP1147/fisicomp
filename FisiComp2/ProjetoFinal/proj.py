d8887c503387605bfbce36694f21f569ef596562
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-05-07 15:39:46 -0400

    Update proj.py

--- FisiComp2/ProjetoFinal/proj.py
+++ FisiComp2/ProjetoFinal/proj.py
@@ -47,60 +47,75 @@ def satellite(center, h, period, M):			# centro (x,y) | Altura em relacao
 	height = h / size_factor					# Converte para a escala
 
 	desloc = 2*m.pi*h							# Deslocamento (circunferencia) - Km
+	mi = G*M 									# Standard gravitational parameter
 
 	R = 0
 	V = (desloc / period)						# Vel - km/s
 	Vh = (desloc / (period/3600))
-	ag = (G*M)/((h*1000)**2)
-	#V = m.sqrt((G*M)/h)
+
+	a = 3
+	b = 1
+	e = m.sqrt(1 - (b**2/a**2))
+	smajor = h*a 								# Semieixo maior
+	sminor = h*b  								# Semieixo menor
 
 	dt = 1/60 								# Variacoes de tempo (dt) e posicao (dx, dy)
 											# para cada frame (60 fps)
 	dx = ( dt / period ) 
-	dy = ( dt / period ) 
+	dy = ( dt / period )
 	#print(dx)
 	if period/60 < 200: per = str(round(period/60, 2))+" min"		# Legenda
 	elif period/60 >= 200: per = str(round(period/3600,2))+" h"
 
-	info = 		"V = "+str(round(Vh, 2))+" km/h\n"
-	info +=		"T = "
-	info +=		per
-	info +=		"\ng = "+str(round(ag, 2))+" m/s²"
-	satinfo = gfx.Text(gfx.Point(x/2, y-30), info)
-	satinfo.setFill(gfx.color_rgb(255,255,255))
-	satinfo.draw(Win)
 																# loop da orbita - segue 
 																# durante o resto da
 																# execucao
 	while R <= desloc:
-		sat = gfx.Circle(gfx.Point(m.cos(X)*height + center[0], m.sin(Y)*height + center[1]), 3)
+
+		dc = m.sqrt((a*m.cos(X) + (a*e**2))**2 + (b*m.sin(Y))**2)*h
+		ag = mi/((dc*1000)**2)
+		Vi = m.sqrt( mi*((2/dc) - (1/smajor)) )
+
+		info = 		"V = "+str(round(Vi, 2))+" km/h\n"
+		info +=		"T = "
+		info +=		per
+		info +=		"\ng = "+str(round(ag, 2))+" m/s²\n"
+		info +=		"Dc = "+str(round(dc, 2))+" Km"
+		satinfo = gfx.Text(gfx.Point(x/2, y-30), info)
+		satinfo.setFill(gfx.color_rgb(255,255,255))
+		satinfo.draw(Win)
+
+		sat = gfx.Circle(gfx.Point(a*m.cos(X)*height + height*(a*e**2) + center[0], b*m.sin(Y)*height + center[1]), 3)
 		sat.setFill(gfx.color_rgb(255,255,255))
 		sat.draw(Win)
 
 		vt = gfx.Line(gfx.Point(m.cos(X)*height + center[0], m.sin(Y)*height + center[1]), gfx.Point(m.cos(X + 30*dx*motion)*height + center[0], m.sin(Y + 30*dy*motion)*height + center[1]))
 		vt.setArrow("last") 
 		vt.setFill(gfx.color_rgb(0,150,0))
-		vt.draw(Win)
+		#vt.draw(Win)
 
 		fg = gfx.Line(gfx.Point(m.cos(X)*height + center[0], m.sin(Y)*height + center[1]), gfx.Point(m.cos(X + dx*motion)*height/(3/2) + center[0], m.sin(Y + dy*motion)*height/(3/2) + center[1]))
 		fg.setArrow("last") 
 		fg.setFill(gfx.color_rgb(0,0,150))
-		fg.draw(Win)
+		#fg.draw(Win)
+
 
 		Win.update()											# Desenha frame
-		X += (dx) * motion
-		Y += (dy) * motion
+		X += (dx) * motion * (h/dc)
+		Y += (dy) * motion * (h/dc)
 		#print(X)
 		#R += dx*h
+		#print("D = {:.4f} Km".format(dc))
 		time.sleep(dt)											# Variacao do tempo
 		sat.undraw()											# Apaga para o 
 																# prox. frame
-		vt.undraw()
-		fg.undraw()
+		#vt.undraw()
+		#fg.undraw()
+		satinfo.undraw()
 
-mv = 3600			# Fator de velocidade - Maior = mais rapido 
-					# (1 = tempo real)
-sf = 50				# Fator de tamanho - Maior = maior `zoom`
+mv = 3600*60			# Fator de velocidade - Maior = mais rapido 
+						# (1 = tempo real)
+sf = 700				# Fator de tamanho - Maior = maior `zoom`
 
 init(800, mv, sf)
 
@@ -111,12 +126,12 @@ Mt = 5.97*(10**24)  		# kg
 c = [ x/2, y/2 ]		# Centro da janela: [ x/2, y/2 ]
 
 # Cria o planeta (Terra) com as dimensoes acima
-#planet("Terra", c, r, M)
-planet("Marte", c, rt, Mt)
+planet("Terra", c, rt, Mt)
+#planet("Marte", c, rm, Mm)
 
 # Dados do satelite
-#h = 35786					# Km - geostacionario
-h = 408					# Km - ISS
+h = 35786					# Km - geostacionario
+#h = 408					# Km - ISS
 #h = 384000                 # Km - Lua
 
 a = (rt + h)*1000 		# m
